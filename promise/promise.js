352c36c788427b761cfaed64e808f06589a2c04e
Ken <396989681@qq.com>
2020-10-20 17:36:00 +0800

    promise

---
+++ promise/promise.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Hjm
+ * @LastEditors: Hjm
+ * @Date: 2020-10-20 09:42:26
+ * @LastEditTime: 2020-10-20 15:44:43
+ * @FilePath: \_exercise\promise\promise.js
+ */
+
+// 1、promise 状态包括pending,onFulfilled,onRejected
+// 2、resolve 可以将pending状态转为onFulfilled，同理~ reject 转为onRejected状态
